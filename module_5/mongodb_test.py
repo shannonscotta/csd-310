bd43931e9042c7d786b8f0534152314eccae3d79
Scott Shannon <scott.a.shannon@outlook.com>
2022-03-08 00:14:33 -0800

    finish mod 5.2 assignment

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,14 @@
+
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.exc81.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+print("")
+print("- - Pytech COlection List - -")
+print(db.list_collection_names())
+print("")
+input("   End of program, press any key to exit...")
